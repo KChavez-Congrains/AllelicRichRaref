82d72aef2754145ddee133f78655adbabc7e1ac0
KChavez-Congrains <veronica.karla@gmail.com>
2017-06-21 22:10:07 -0300

    Update AllelicRichRaref.R

--- AllelicRichRaref.R
+++ AllelicRichRaref.R
@@ -7,18 +7,18 @@ AllelicRichRaref <- function(data){
   for(i in 1:maxsample){                                                  #Resampling
     G <- 2*i                                                              #Number of genes
     loci <- ncol(data)/2
-    for(j in 1:loci){                                                     #Analise each locus
+    for(j in 1:loci){                                                     #Analysis each locus
       data2 <- data[ ,((j*2)-1):(j*2)]                                    #Select the columns of the locus
       K <- as.data.frame(table(unlist(data2)))                            #Calculate the frequency of each allele
       K$Var1 <- as.numeric(K$Var1)
       my_vector = c()
-      for(k in K$Var1){                                                   #Analise each allele                                
+      for(k in K$Var1){                                                   #Analysis each allele                                
         Ni <- nrow(data)*2
         Nik <- K[K$Var1==k,]$Freq
         numerator <- choose((Ni-Nik),G)                                   #Number of combinations of G genes that not include the allele k
         denominator <- choose((Ni),G)                                     #Number of possible combinations of G genes taken from the sample of Ni genes
         Pi <- numerator/denominator                                       #Probability that allele k does not occur in a sample of G genes chosen as reference
-        nPi <- 1-Pi                                                       #Probability that allele k occur
+        nPi <- 1-Pi                                                       #Probability that allele k occurs
         my_vector[length(my_vector)+1] <- nPi
       }
       E <- sum(my_vector)                                                 #Allelic richness at one locus
